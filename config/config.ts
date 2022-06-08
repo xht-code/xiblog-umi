b7f3b53433793ba777ec28d74916ff7703b26960
XHT-CODE <xht_code@qq.com>
2022-06-09 00:26:07 +0800

    feat: update config

--- config/config.ts
+++ config/config.ts
@@ -1,15 +1,14 @@
 import routes from './routes'
 
 export default {
+  history: { type: 'hash' },
   routes,
   antd: {},
   request: {},
   tailwindcss: {},
   deadCode: {},
   targets: { chrome: 51 },
-  mfsu: {
-    esbuild: true,
-  },
+  mfsu: { esbuild: true },
   lessLoader: {
     modifyVars: {
       hack: `true; @import "~@/styles/variables/index.less";`,
