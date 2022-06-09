fe8992ae3d378275343d3c3bd0cd40f922f3b8b8
XHT-CODE <xht_code@qq.com>
2022-06-09 13:45:14 +0800

    feat: husky test 4

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'umi'
 import GlobalHeader from './header'
 
 export default function GlobalLayout() {
-  console.log('husky test 3')
+  console.log('husky test 4')
   return (
     <div className='!min-h-[100vh] flex flex-col'>
       <GlobalHeader />
